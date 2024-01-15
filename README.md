3c2fba549f154cd7e98bf5408aa2142fa0362c34
0xKilty <iankilty1@gmail.com>
2024-01-15 16:53:41 -0700

    progress

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Attempting to recreate the output of `objdump`
 
 ```
-objdump -d -M intel test
+objdump -d -M intel primes
 ```
 
 However, `objdump` follows a linear sweep patteren in which it may not always produce the correct results in differentiating between data and code. Recursive disassembly is the solution to this and attempts to improve upon a linear sweep.
--- sword.c
+++ sword.c
@@ -1,10 +1,45 @@
+/*
+                                ,-.
+                               ("O_)
+                              / `-/
+                             /-. /
+                            /   )
+                           /   /  
+              _           /-. /
+             (_)"-._     /   )
+               "-._ "-'""( )/    
+                   "-/"-._" `. 
+                    /     "-.'._
+                   /\       /-._"-._
+    _,---...__    /  ) _,-"/    "-(_)
+___<__(|) _   ""-/  / /   /
+ '  `----' ""-.   \/ /   /
+               )  ] /   /
+       ____..-'   //   /                       )
+   ,-""      __.,'/   /   ___                 /,
+  /    ,--""/  / /   /,-""   """-.          ,'/
+ [    (    /  / /   /  ,.---,_   `._   _,-','
+  \    `-./  / /   /  /       `-._  """ ,-'
+   `-._  /  / /   /_,'            ""--"
+       "/  / /   /"         
+       /  / /   /
+      /  / /   /
+     /  |,'   /  
+    :   /    /
+    [  /   ,'
+    | /  ,'
+    |/,-'
+    P'
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 #include <elf.h>
 
-Elf64_Ehdr read_elf_file(int fd) {
+Elf64_Ehdr read_elf_header(int fd) {
     Elf64_Ehdr elf_header;
     ssize_t bytesRead = read(fd, &elf_header, sizeof(Elf64_Ehdr));
     if (bytesRead != sizeof(Elf64_Ehdr)) {
@@ -26,6 +61,20 @@ Elf64_Ehdr read_elf_file(int fd) {
     return elf_header;
 }
 
+Elf64_Shdr read_section_header(int fd, Elf64_Ehdr elf_header) {
+    Elf64_Shdr section_header;
+    lseek(fd, elf_header.e_shoff + elf_header.e_shstrndx * elf_header.e_shentsize, SEEK_SET);
+    read(fd, &section_header, sizeof(section_header));
+    return section_header;
+}
+
+char* get_section_names(int fd, Elf64_Shdr section_header) {
+    char* section_names = malloc(section_header.sh_size);
+    lseek(fd, section_header.sh_offset, SEEK_SET);
+    read(fd, section_names, section_header.sh_size);
+    return section_names;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <elf_executable>\n", argv[0]);
@@ -40,19 +89,9 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    Elf64_Ehdr elf_header = read_elf_file(fd);
-
-    printf("entry point %lx\n", elf_header.e_entry);
-
-    // Read section header
-    Elf64_Shdr section_header;
-    lseek(fd, elf_header.e_shoff + elf_header.e_shstrndx * elf_header.e_shentsize, SEEK_SET);
-    read(fd, &section_header, sizeof(section_header));
-
-    // Read section names
-    char* section_names = malloc(section_header.sh_size);
-    lseek(fd, section_header.sh_offset, SEEK_SET);
-    read(fd, section_names, section_header.sh_size);
+    Elf64_Ehdr elf_header = read_elf_header(fd);
+    Elf64_Shdr section_header = read_section_header(fd, elf_header);
+    char* section_names = get_section_names(fd, section_header);
     
     // Print section names
     for (int i = 0; i < elf_header.e_shnum; i++){
@@ -61,7 +100,10 @@ int main(int argc, char *argv[]) {
 
         // Check if section is the .text section
         if (section_header.sh_type == SHT_PROGBITS && section_header.sh_flags & SHF_EXECINSTR) {
-            printf("%s 0x%lx\n", section_names + section_header.sh_name, (unsigned long)section_header.sh_addr);
+            // Print the section name along with the addr
+            printf("\n%s 0x%lx\n", section_names + section_header.sh_name, (unsigned long)section_header.sh_addr);
+
+            // Read the hex data
             unsigned char* text_section_data = (unsigned char*)malloc(section_header.sh_size);
             if (text_section_data == NULL) {
                 perror("Memory allocation error");
@@ -70,10 +112,15 @@ int main(int argc, char *argv[]) {
             lseek(fd, section_header.sh_offset, SEEK_SET);
             read(fd, text_section_data, section_header.sh_size);
 
+            for (int j = 0; j < section_header.sh_size; j++) {
+                printf("%x ", text_section_data[j]);
+            }
+
             free(text_section_data);
         }
     }
 
+    printf("\n");
     free(section_names);
     close(fd);
     return 0;
